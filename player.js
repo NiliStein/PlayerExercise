c2bbcc47f9162ec3fbeeb35465376182817003b4
NiliStein <nilistn@gmail.com>
2020-09-20 14:07:43 +0300

    Fetched text from url
    + Got jsonp content from the text

--- player.js
+++ player.js
@@ -6,19 +6,37 @@
 	function validate_website() {
 		let current_url = window.location.href;
 		if (current_url == 'https://www.google.com/'){
-			//TODO: fetch and parse jsonp
 			console.log('Webpage is: https://www.google.com/');
 			return true;
 		}
 		console.error('Can only be run on https://www.google.com/');
 		return false;
-	}		
+	}
 	
 	function showTooltip() {
 		//Oracle's jsonp guide url:
 		const jsonp_url = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867';
 		//TODO: Fetch and parse jsonp, use window.jQuery for that
-		alert('Success!');
+
+		//get as text from url:
+		let fetched_text = window.jQuery.get(jsonp_url, function(data){
+			console.log('loading text succeeded');
+
+			//get substring resides in __5szm2kaj( ... ): 
+			let first_index = "__5szm2kaj(".length;
+			let last_index = data.length - 1;
+			let jsonp_str = data.substring(first_index, last_index);
+
+			console.log('Data substring: ' + jsonp_str);
+
+
+		}, "text")
+		.fail(function(jqXHR, textStatus, errorThrown) {
+			console.log("error " + textStatus);
+			console.log("incoming Text " + jqXHR.responseText);
+			console.log(errorThrown);
+		});
+		
 	}
 	
 	//main function to run in entry point:
