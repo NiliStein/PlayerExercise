c3c38aa5a88e6edd09ec7dfd944e9aa6244166e8
NiliStein <nilistn@gmail.com>
2020-09-25 14:35:19 +0300

    CSS Works!
    + Click events work
    - Back button is cut(?)
    - Some more refinements need to be done

--- Extension/player.js
+++ Extension/player.js
@@ -12,15 +12,37 @@
 		console.error('Can only be run on https://www.google.com/');
 		return false;
 	}
-	
+		
 	function showTooltip() {
+		
 		//Oracle's jsonp guide url:
 		const jsonp_url = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867';
 		//TODO: Fetch and parse jsonp, use window.jQuery for that
 
 		//get as text from url:
-		window.jQuery.get(jsonp_url, function(data){
+		$.get(jsonp_url, function(data){
 			console.log('loading text succeeded');
+			
+			//define click handlers:
+			function onCloseBtnClick() {
+				alert("close button clicked");
+			}
+			
+			function onRemindLaterBtnClick() {
+				alert("remind later button clicked");
+			}
+			
+			function onBackBtnClick() {
+				alert("back button clicked");
+			}
+			
+			function onNextLinkClick() {
+				alert("next clicked");
+			}
+
+			function wrap_with_div(html_data, div_class) {
+				return "<div class='" + div_class + "'>" + html_data + "</div>";
+			}
 
 			//get substring resides in __5szm2kaj( ... ): 
 			let first_index = "__5szm2kaj(".length;
@@ -29,27 +51,57 @@
 
             let jsonp_data = JSON.parse(jsonp_str);
 			
-			//Inject html and css:
-			let tip_html = jsonp_data.data.tiplates.tip;
-			let hover_tip_html = jsonp_data.data.tiplates.hoverTip;
-			let css_str = jsonp_data.data.css;
+			$(document).ready(function() {
 			
-			$("body").append(tip_html);
+				let css_str = jsonp_data.data.css;
+				let style = document.createElement('style');
+				style.innerHTML = css_str;
+				$("body").append(style);
+				
+				// Check if type is tip or CloseScenario
+				
+				// Get current_step
+				
+				let current_step = jsonp_data.data.structure.steps[0];
+				let current_placement = current_step.action.placement;
+				
+				//Inject html and css:
+				let tip_html = jsonp_data.data.tiplates.tip;
+				let hover_tip_html = jsonp_data.data.tiplates.hoverTip;
 			
-			//get elements of the tip:
-			let close_btn = $("button[aria-label='Close']");	
-			let remind_later_btn = $("button[data-iridize-role='laterBt']");
-			let back_btn = $("button[data-iridize-role='prevBt']");
-			let next_link = $("a[data-iridize-role='nextBt']");
-			let step_info = $("span[class='steps-count']");
-			let current_step = step_info.children[0];
-			let total_steps = step_info.children[1];
+				// Wrap tip_html with the corresponding div class
+				
+				let tip_class = ["tooltip", "in", current_placement, current_step.action.classes].join(" ");
+				
+				let wrapped_tip_html = wrap_with_div(
+											wrap_with_div(
+												wrap_with_div(tip_html, tip_class)
+													, "panel-container")
+											, "sttip");
+				// let wrapped_hover_tip_html = wrap_with_div(wrap_with_div(hover_tip_html, "tooltip"), "sttip");
 			
-			
-			let style = document.createElement('style');
-			style.type = 'text/css';
-			style.innerHTML = css_str;
-			$("head").append(style);			
+				$(current_step.action.selector).parent().append(wrapped_tip_html);
+				
+				//get elements of the tip:
+				let tip_div = $("div[aria-label='Steps']");
+				
+				// Assumes there's only one object for each of the following attributes values 
+				let close_btn = $("button[aria-label='Close']");	
+				let remind_later_btn = $("button[data-iridize-role='laterBt']");
+				let back_btn = $("button[data-iridize-role='prevBt']");
+				let next_link = $("a[data-iridize-role='nextBt']");
+				
+				let step_info = $("span[class='steps-count']");
+				let current_step_number = step_info.children[0];
+				let total_steps = step_info.children[1];
+				
+				//set on click functions:	
+				close_btn.click(onCloseBtnClick);
+				remind_later_btn.click(onRemindLaterBtnClick);					
+				back_btn.click(onBackBtnClick);					
+				next_link.click(onNextLinkClick);	
+				
+			});				
 
 		}, "text")
 		.fail(function(jqXHR, textStatus, errorThrown) {
@@ -60,6 +112,8 @@
 		
 	}
 	
+	
+	
 	//main function to run in entry point:
 	function main() {
 		
@@ -67,16 +121,7 @@
 			showTooltip();
 		}
 	}
-	
+		
 	main();
-    //Load the script
-//    var script = document.createElement("SCRIPT");
-//    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
-//   script.type = 'text/javascript';
-//    script.onload = function() {
-		//entry point:
-//		main();
-		//end of main
-//    };
-//    document.getElementsByTagName("head")[0].appendChild(script);
+
 })();
